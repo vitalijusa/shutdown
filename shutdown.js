645a8a7dee34e5ccad9cd484fe2470bdcf2fe718
Vitalijus Agejevas <VIAG@COWI.com>
2017-04-18 08:56:02 +0300

    added packages file

--- shutdown.js
+++ shutdown.js
@@ -1,8 +1,19 @@
 const os = require('os');
-const jsonfile = require('jsonfile');
 const configUtil = require('./configUtil.js');
 
 var configFile = "param.json";
 
+var check = function() {
+    var params = configUtil.loadConfig(configFile);
+    var osUptime = os.uptime();
+
+    console.log(os.uptime());
+    console.log(params);
+
+    configUtil.saveConfig(params);
+};
+
+
 console.log(os.uptime());
 console.log(configUtil.loadConfig(configFile));
+
