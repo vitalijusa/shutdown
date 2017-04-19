b91b0845d3a5f86308e464c29cfbb7cd0f92814a
Vitalijus Agejevas <VIAG@COWI.com>
2017-04-19 16:32:35 +0300

    Updating uptime; Pubnub

--- pubnub.js
+++ pubnub.js
@@ -1,1 +1,60 @@
-const pubnub = require('pubnub');
+const PubnubClass = require('pubnub');
+
+module.exports = {
+    Pubnub: Pubnub
+};
+
+function Pubnub(userId, publishKey, subscribeKey) {
+    var self = this;
+    var channel = "shutdown";
+
+    var pubnub = new PubnubClass({
+        publishKey: publishKey,
+        subscribeKey: subscribeKey,
+        uuid: userId
+    });
+
+    pubnub.addListener({
+        status: function(statusEvent){
+            console.log("status changed:", statusEvent);
+
+            if (statusEvent.category === "PNConnectedCategory") {
+                var newState = {
+                    name: 'presence-tutorial-user',
+                    timestamp: new Date()
+                };
+                pubnub.setState(
+                    { 
+                        channels: [channel],
+                        state: newState 
+                    }
+                );
+            }
+        },
+        message: function(message){
+            console.log(message)
+        },
+        presence: function(presenceEvent){
+            console.log(presenceEvent);
+        }
+    })
+
+    pubnub.subscribe({
+        channels: [channel],
+        withPresence: true
+    });
+
+
+
+    self.hereNow = function() {
+        pubnub.hereNow(
+            {
+                channels: [channel],
+                includeState: true
+            },
+            function(status, response){
+                console.log(response);
+            }
+        );
+    };
+}
--- shutdown.js
+++ shutdown.js
@@ -1,21 +1,51 @@
 const os = require('os');
 const moment = require('moment');
 const configUtil = require('./configUtil.js');
+const Pubnub = require('./pubnub.js').Pubnub;
 
+const CHECK_INTERVAL = 5*60 * 1000; // 5 minutes
 var configFile = "param.json";
 
 var check = function() {
     var params = configUtil.loadConfig(configFile);
     var uptime = os.uptime();
     var now = moment();
+    var lastCheck = moment(params.lastCheck);
+    var pubnub = new Pubnub("Checker", params.pubnubPublishKey, params.pubnubSubscribeKey);
 
-    params.firstCheck = now.format();
-    params.lastCheck = now.valueOf();
-    params.uptime = uptime;
+    if (!lastCheckWasToday(lastCheck, now)) {
+        // reset params
+        params.uptime = uptime;
+        params.firstCheck = now.format();        
+    }
+
+    params.uptime = getUpdatedUptime(params.uptime, uptime, lastCheck, now.valueOf());
+    params.lastCheck = now.valueOf();    
 
     console.log(params);
     configUtil.saveConfig(configFile, params);
+
+    pubnub.hereNow();
 };
-
-
 check();
+
+function lastCheckWasToday(lastCheck, now) {
+    return lastCheck.dayOfYear() === now.dayOfYear();
+}
+
+function getUpdatedUptime(lastUptime, uptime, lastCheck, now) {
+    var newUptime = 0;
+    var buffer = 5000;
+
+    if (lastUptime <= uptime) {
+        newUptime = uptime;
+    } else {
+        if (lastCheck + CHECK_INTERVAL + buffer > now) {
+            newUptime = lastUptime + checkInterval;
+        } else {
+            newUptime = lastUptime + uptime;
+        }
+    }
+    return newUptime;
+}
+
